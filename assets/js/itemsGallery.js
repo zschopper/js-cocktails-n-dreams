fb265bf9c6049b55c15d522ac44292f6531a13df
zschopper <zschopper@gmail.com>
2023-04-26 10:56:25 +0200

    Only the "Flip it" button flips the card (instead of entire card surface) Hide horizontal scrollbar. Static test displays when all items have been filtered out

--- assets/js/itemsGallery.js
+++ assets/js/itemsGallery.js
@@ -43,83 +43,85 @@ export class ItemsGallery {
     renderItems() {
         let html = '';
         let max = 20;
-        for (let item of this.drinkList.filteredItemList) {
-            let ingredients = '<ul>' + item.ingredients.map(e => `<li>${e.ingredient}</li>`).join("") + '</ul>';
-            // if (!--max) break;
-            html +=
-                '<div class="card-wrapper">' +
-                `  <div class="card" data-id="${item.id}">` +
-                '    <div class="card-face card-face-front">' +
-                `      <div class="card-header">${item.name}</div>` +
-                `      <div class="card-body"><p class="card-text"> <img src="${item.image}" alt="${item.name}"/> </p></div>` +
-                '      <div class="card-footer">' +
-                '        <div class="row">' +
-                '          <div class="col-6 text-end"><a href="#" class="btn btn-sm btn-success flip"> Flip it!</a> or <a href="#" class="btn btn-sm btn-success order">Order it!</a></div>' +
-                '        </div>' +
-                '      </div>' +
-                '    </div>' +
-                '    <div class="card-face card-face-back">' +
-                `      <div class="card-header">${item.name}</div>` +
-                `      <div class="card-body"><p class="card-text"> Ingredients </p>${ingredients}</div>` +
-                '      <div class="card-footer">' +
-                '        <div class="row">' +
-                '          <div class="col-6 text-end"><a href="#" class="btn btn-sm btn-success flip">Flip back and order it!</a></div>' +
-                '        </div>' +
-                '      </div>' +
-                '    </div>' +
-                '  </div>' +
-                '</div>';
-        }
+        if (!this.drinkList.filteredItemList.length) {
+            $(this.container).html('<div class="gallery-is-empty">No item in the list.</div>');
 
-        let isDown = false;
-        let startX;
-        let scrollLeft;
+        } else {
+            for (let item of this.drinkList.filteredItemList) {
+                let ingredients = '<ul>' + item.ingredients.map(e => `<li>${e.ingredient}</li>`).join("") + '</ul>';
+                 // if (!--max) break;
+                html +=
+                    '<div class="card-wrapper">' +
+                    `  <div class="card" data-id="${item.id}">` +
+                    '    <div class="card-face card-face-front">' +
+                    `      <div class="card-header">${item.name}</div>` +
+                    `      <div class="card-body"><p class="card-text"> <img src="${item.image}" alt="${item.name}"/> </p></div>` +
+                    '      <div class="card-footer">' +
+                    '        <div class="row">' +
+                    '          <div class="col-6 text-end"><a href="#" class="btn btn-sm btn-success flip"> Flip it!</a> or <a href="#" class="btn btn-sm btn-success order">Order it!</a></div>' +
+                    '        </div>' +
+                    '      </div>' +
+                    '    </div>' +
+                    '    <div class="card-face card-face-back">' +
+                    `      <div class="card-header">${item.name}</div>` +
+                    `      <div class="card-body"><p class="card-text"> Ingredients </p>${ingredients}</div>` +
+                    '      <div class="card-footer">' +
+                    '        <div class="row">' +
+                    '          <div class="col-6 text-end"><a href="#" class="btn btn-sm btn-success flip">Flip back and order it!</a></div>' +
+                    '        </div>' +
+                    '      </div>' +
+                    '    </div>' +
+                    '  </div>' +
+                    '</div>';
+            }
 
-        $(this.container).html(html);
+            let isDown = false;
+            let startX;
+            let scrollLeft;
 
-        $(this.container).on('mousedown', (e) => {
-            let container = $(this.container)[0]
-            isDown = true;
-            $(this.container).addClass('active');
-            startX = e.pageX - container.offsetLeft;
-            scrollLeft = container.scrollLeft;
-        });
+            $(this.container).html(html);
 
-        $(this.container).on('mouseleave', () => {
-            isDown = false;
-            $(this.container).removeClass('active');
-        });
-
-        $(this.container).on('mouseup', () => {
-            isDown = false;
-            $(this.container).removeClass('active');
-        });
-
-        $(this.container).on('mousemove', (e) => {
-            if (isDown) {
+            $(this.container).on('mousedown', (e) => {
                 let container = $(this.container)[0]
-                let x = e.pageX - container.offsetLeft;
-                let walk = (x - startX) * 3;
-                container.scrollLeft = scrollLeft - walk;
-                e.preventDefault();
-            }
-        });
+                isDown = true;
+                $(this.container).addClass('active');
+                startX = e.pageX - container.offsetLeft;
+                scrollLeft = container.scrollLeft;
+            });
 
-        $(this.container + " .order").on('click', (e) => {
-            let id = $(e.target).closest("[data-id]").data("id");
-            let item = this.drinkList.findItemById(id);
-            if (item) {
-                Basket.instance.addItem(item, 1);
-            }
-        });
+            $(this.container).on('mouseleave', () => {
+                isDown = false;
+                $(this.container).removeClass('active');
+            });
 
-        $(this.container + " .card").on('click', (e) => {
-            // $(e.target).toggle("is-flipped");
-            let card = $(e.target).closest(".card");
-            card.toggleClass("is-flipped");
+            $(this.container).on('mouseup', () => {
+                isDown = false;
+                $(this.container).removeClass('active');
+            });
 
-            console.log("fliporder click", card);
-        });
+            $(this.container).on('mousemove', (e) => {
+                if (isDown) {
+                    let container = $(this.container)[0]
+                    let x = e.pageX - container.offsetLeft;
+                    let walk = (x - startX) * 3;
+                    container.scrollLeft = scrollLeft - walk;
+                    e.preventDefault();
+                }
+            });
+
+            $(this.container + " .order").on('click', (e) => {
+                let id = $(e.target).closest("[data-id]").data("id");
+                let item = this.drinkList.findItemById(id);
+                if (item) {
+                    Basket.instance.addItem(item, 1);
+                }
+            });
+
+            $(this.container + " .card .flip").on('click', (e) => {
+                let card = $(e.target).closest(".card");
+                card.toggleClass("is-flipped");
+            });
+        }
 
     }
 
